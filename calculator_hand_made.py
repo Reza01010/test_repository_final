954d3cf742ad6e5a3f2287b5670a114d08d946d7
reza <hi95534reza@gmail.com>
2023-01-21 17:20:07 +0330

    add butom-Func in calculator

--- calculator_hand_made.py
+++ calculator_hand_made.py
@@ -7,20 +7,73 @@ index_operator = 0
 
 str_ing = ""
 
+
 def func_appnd(a):
-    pass
+    global index_dat
+    global index_operator
+    global var_str
+    global str_ing
+    
+    
+    
+    if a == "." and index_dat < index_operator :
+        str_ing = str_ing + a
+        var_str.set(str_ing)
+        index_dat = len(str_ing)
+        
+    
+    
+    
+    try:
+        if a in ['+', '-', '/', '*'] and str_ing[-1] in ['+', '-', '/', '*']:
+            j = 0
+            for i in str_ing:
+                j += 1
+            j -= 1
+            a = ''
+            for i in range(int(j)):
+                
+                a += str_ing[i]
+                
+            str_ing = a
+            var_str.set(str_ing)
+            index_operator = len(str_ing)
+        if a in ["(",")","7","8","9",'4',"6","1",'2',"3",'0','+', '-', '/', '*','.']:
+            str_ing = str_ing + a
+            var_str.set(str_ing)
+    except IndexError:
+        var_str.set('number vared knid')
     
 
 
 def func_delet(*args):
-    pass
+    global var_str
+    global str_ing
+    j = 0
+    for i in str_ing:
+        j += 1
+    j -= 1
+    a = ''
+    for i in range(int(j)):
+        
+        a += str_ing[i]
+        
+    str_ing = a
+    var_str.set(str_ing)
 
 def natije(*args):
-    pass
+    global str_ing
+    global var_str
+    number = str(eval(str_ing))
+    var_str.set(number)
+    str_ing = number
     
     
 def func_ful_delet():
-    pass
+    global var_str
+    global str_ing
+    str_ing = ""
+    var_str.set(str_ing)
     
 
 list_str = [["(", lambda: func_appnd('(')], [")", lambda: func_appnd(")")],\
@@ -39,10 +92,10 @@ var_str = tk.StringVar()
 windo.geometry("369x479")
 windo.configure(background = '#D0DEED')
 lbl_1 = tk.Label(text="................................."*8+'...',bd=50,font=('Jokerman', 1))
-lbl_0 = tk.Label(text="      "*8+'    ',bg="#CADCEF",bd=40,font=('Bauhaus 93', 16))
+lbl_0 = tk.Label(text="      "*8+'    ',bg="#CADCEF",bd=40,font=('Jokerman', 16))
 lbl_0.grid(row=1,column=0,columnspan=4,sticky='we')
 lbl_1.grid(row=0,column=0,columnspan=4)
-lbl_2 = tk.Label(textvariable=var_str,justify='right',bd=40,bg="#CADCEF",font=('Bauhaus 93', 16))
+lbl_2 = tk.Label(textvariable=var_str,justify='right',bd=40,bg="#CADCEF",font=('Jokerman', 16))
 lbl_2.grid(row=1,columnspan=4,)
 
 l = -1
@@ -56,5 +109,6 @@ for i,j in list_str :
 
 # -----------------------------
     tk.Button(master=windo,background=a, height=1,width=5,bd=4,relief=tk.RIDGE, text=i, font=('Bauhaus 93', 16),command=j,).grid(sticky='we',row= ( 2 + l // 4),padx=1, pady=4, column= l % 4)
+
 
 tk.mainloop()
